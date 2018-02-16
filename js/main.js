62de8b42a9f7256b867ec3c29fb5e51578d1b419
Dion Halfhide <1035307@idcollege.nl>
2018-02-16 09:43:33 +0100

    Final push
    Site is af

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+ //smooth scrolling
+    var scrollLink = $(".scroll");
+     
+    scrollLink.click(function(e){
+
+        e.preventDefault();
+        $("body,html").animate({
+            scrollTop: $(this.hash).offset().top-50},1500 );
+    });
+    //Active link switching
+    $(window).scroll(function(){
+        var scrollbarLocation = $(this).scrollTop();
+      
+        scrollLink.each(function(){
+          
+            var sectionOffset = $(this.hash).offset().top -70;
+            if (sectionOffset <= scrollbarLocation ){
+$(this).parent().addClass('active');
+$(this).parent().siblings().removeClass('active');
+            }
+        });
+        
+        
+    });
+    
+});
